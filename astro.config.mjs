43532ef12e47dae5bf72e42b735d66a2b6bf3ce7
maxim <bozzhik@ya.ru>
2024-07-08 20:09:51 +0300

    add `react` in project
    pnpm dlx astro add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import {defineConfig} from 'astro/config'
 
+import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
 export default defineConfig({
   server: {
     port: 2000,
     open: true,
   },
   site: 'https://web-plakat.bozzhik.ru',
-
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 })
