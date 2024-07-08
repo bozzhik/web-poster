693ee0ccb6e1cdca8da3b087c8fde05ecdf4cb54
maxim <bozzhik@ya.ru>
2024-07-08 19:35:54 +0300

    extend `tsconfig` with `aliases` & update `astro` config
    https://docs.astro.build/en/guides/typescript/#import-aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    port: 2000,
+    open: true,
+  },
+  site: 'https://web-plakat.bozzhik.ru',
+
   integrations: [tailwind()],
 })
