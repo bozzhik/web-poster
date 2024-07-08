7504bdf785efa04bae4a31cd0464d1a1f8588403
maxim <bozzhik@ya.ru>
2024-07-08 21:04:03 +0300

    init `shadcn-ui` for project
    https://ui.shadcn.com/docs/installation/astro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,9 @@ import type {Config} from 'tailwindcss'
 import plugin from 'tailwindcss/plugin'
 
 const config = {
+  darkMode: ['class'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  prefix: '',
   theme: {
     screens: {
       xl: {max: '1536px'},
@@ -12,9 +14,58 @@ const config = {
     },
     extend: {
       colors: {
-        custom: {
-          primary: '#FC4E2B',
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: {
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)',
         },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          foreground: 'var(--secondary-foreground)',
+        },
+        destructive: {
+          DEFAULT: 'var(--destructive)',
+          foreground: 'var(--destructive-foreground)',
+        },
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent)',
+          foreground: 'var(--accent-foreground)',
+        },
+        popover: {
+          DEFAULT: 'var(--popover)',
+          foreground: 'var(--popover-foreground)',
+        },
+        item: {
+          DEFAULT: 'var(--item)',
+          foreground: 'var(--item-foreground)',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: {height: '0'},
+          to: {height: 'var(--radix-accordion-content-height)'},
+        },
+        'accordion-up': {
+          from: {height: 'var(--radix-accordion-content-height)'},
+          to: {height: '0'},
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
@@ -23,6 +74,7 @@ const config = {
       api.matchUtilities({s: (value) => ({width: value, height: value})}, {values: api.theme('width')})
     }),
     require('@tailwindcss/typography'),
+    require('tailwindcss-animate'),
   ],
 } satisfies Config
 
