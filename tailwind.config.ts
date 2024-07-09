10218577a0692793064732e383c0dc66ec7ed7e9
maxim <bozzhik@ya.ru>
2024-07-09 03:13:14 +0300

    disable `strict` mode in `tsconfig`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,11 +55,6 @@ const config = {
           foreground: 'var(--item-foreground)',
         },
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
       keyframes: {
         'accordion-down': {
           from: {height: '0'},
