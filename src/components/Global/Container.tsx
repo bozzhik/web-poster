b4a4aee964b6f7247c0757fdfd48da3459021872
maxim <bozzhik@ya.ru>
2024-08-02 02:26:56 +0300

    configure `websiteBox` for `Container` Global component

--- src/components/Global/Container.tsx
+++ src/components/Global/Container.tsx
@@ -7,14 +7,14 @@ interface Props {
   className?: string
 }
 
-export const websiteWidth = {
+export const websiteBox = {
   default: 'mx-auto flex-auto',
-  '1/2': 'w-[50%] sm:w-auto sm:mx-2',
-  '4/5': 'w-[94%] sm:w-auto sm:mx-2',
+  '1/2': '',
+  '4/5': 'mx-[25px] sm:mx-2',
 }
 
 export default function Container({children, width = '4/5', padding = true, className}: Props) {
-  const containerStyles = `${websiteWidth.default} ${websiteWidth[width]} ${padding ? 'pt-28 sm:pt-16' : ''}`
+  const containerStyles = `${websiteBox.default} ${websiteBox[width]} ${padding ? 'pt-28 sm:pt-16' : ''}`
 
-  return <main className={cn(containerStyles, className)}>{children}</main>
+  return <main className={cn(containerStyles, 'mx-6', className)}>{children}</main>
 }
--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,5 +1,5 @@
 import {websitePaths} from '@/lib/constants'
-import {websiteWidth} from './Container'
+import {websiteBox} from './Container'
 
 interface Props {
   width?: '1/2' | '4/5'
@@ -10,9 +10,9 @@ export default function Header({width = '4/5'}: Props) {
 
   return (
     <header className="fixed w-full pt-5 sm:pt-2 text-[22px] sm:text-lg font-book z-50">
-      <nav className={`flex items-center justify-between bg-item mx-auto p-4 sm:p-1.5 rounded-[20px] sm:rounded-xl ${websiteWidth[width]}`}>
+      <nav className={`flex items-center justify-between bg-item p-4 sm:p-1.5 rounded-[20px] sm:rounded-xl ${websiteBox[width]}`}>
         <div className="flex items-center gap-8 sm:gap-3">
-          <a href="/" className="w-[70px] h-10 sm:w-14 sm:h-8 duration-300 rounded-[12px] sm:rounded-lg bg-secondary hover:bg-primary-hover"></a>
+          <a href="/" className="w-[70px] h-10 sm:w-14 sm:h-8 duration-300 rounded-[12px] sm:rounded-lg bg-primary hover:bg-primary-hover"></a>
 
           <div className="space-x-5 sm:space-x-0 mt-[-3px] sm:mt-0">
             {Object.entries(main).map(([key, {text, link}]) => (
