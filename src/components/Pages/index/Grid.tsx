62f62ebfea6953a1ea07852daafe89b86a78f28a
maxim <bozzhik@ya.ru>
2024-08-02 20:11:08 +0300

    init `HeroCell` into new `GridBlocks` child index component

--- src/components/Pages/index/Grid.tsx
+++ src/components/Pages/index/Grid.tsx
@@ -1,27 +1,28 @@
 import {cn} from '@/lib/utils'
+import {HeroCell} from '##/index/GridBlocks'
+
+const gridConfig = {
+  core: 'grid-cols-10 grid-rows-10 sm:flex sm:flex-col',
+  child: {
+    hero: 'col-span-5 row-span-6',
+    pluses: 'col-span-2 row-span-8',
+    builder: 'col-span-3 row-span-8',
+
+    figma: 'col-span-2 row-span-4',
+    showcase: 'col-span-3 row-span-4',
+    docs: 'col-span-3 row-span-2',
+    team: 'col-span-2 row-span-2',
+  },
+  gap: 'gap-5 sm:gap-2',
+}
+export const {core, child, gap} = gridConfig
+const blockStyles = 'grid place-items-center px-5 text-center font-book text-primary bg-item rounded-2xl text-xl sm:min-h-[15vh]'
 
 export default function Grid() {
-  const gridConfig = {
-    core: 'grid-cols-10 grid-rows-10 sm:flex sm:flex-col',
-    child: {
-      hero: 'col-span-5 row-span-6',
-      pluses: 'col-span-2 row-span-8',
-      builder: 'col-span-3 row-span-8',
-
-      figma: 'col-span-2 row-span-4',
-      showcase: 'col-span-3 row-span-4',
-      docs: 'col-span-3 row-span-2',
-      team: 'col-span-2 row-span-2',
-    },
-    gap: 'gap-5 sm:gap-2',
-  }
-  const {core, child, gap} = gridConfig
-
-  const blockStyles = 'grid place-items-center px-5 text-center font-book text-primary bg-item rounded-2xl text-xl sm:min-h-[15vh]'
-
   return (
     <section className={`grid ${core} ${gap} h-[86vh] xl:h-[83vh] sm:h-auto`}>
-      <div className={cn(child.hero, gap, blockStyles)}>основной</div>
+      <HeroCell />
+
       <div className={cn(child.pluses, gap, 'grid grid-rows-2')}>
         <div className={blockStyles}>готовый репозиторий?</div>
         <div className={blockStyles}>возможность взять код</div>
---
+++ src/components/Pages/index/GridBlocks.tsx
@@ -0,0 +1,27 @@
+import {cn} from '@/lib/utils'
+import {child, gap} from '##/index/Grid'
+const blockStyles = 'bg-item rounded-2xl text-xl sm:min-h-[15vh]'
+
+import HeroGridImage from '%/images/index/hero-grid.svg'
+import HeroDeviceImage from '%/images/index/hero-device.png'
+
+export function HeroCell() {
+  return (
+    <div className={cn('relative overflow-hidden', [child.hero, gap, blockStyles])}>
+      <div className="relative z-20 flex flex-col justify-between h-full p-6 pt-5 sm:gap-4 xl:p-5 xl:pt-4 sm:pb-3 sm:text-center">
+        <h1 className="font-bold !leading-[90%] tracking-tight text-primary uppercase text-[110px] xl:text-[85px] sm:text-6xl">
+          Ваш <br /> плакат <br /> в вебе
+        </h1>
+
+        <h2 className="text-2xl !leading-tight tracking-normal xl:text-xl sm:text-lg font-book">
+          Одностраничный сайт, сочетающий <br className="sm:hidden" /> дизайн плаката и&nbsp;веб-анимацию
+        </h2>
+      </div>
+
+      <img className="absolute sm:relative sm:inset-auto sm:scale-110 top-0 z-10 object-cover h-full right-[-290px]" src={HeroDeviceImage.src} />
+      <img className="absolute top-0 object-cover w-full h-full" src={HeroGridImage.src} />
+
+      <div className="absolute z-10 bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black/15 via-black/10 to-[#25252501]"></div>
+    </div>
+  )
+}
