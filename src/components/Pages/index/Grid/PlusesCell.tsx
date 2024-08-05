adc352824fec01e7ea46d52bb84eb07d73c5b542
maxim <bozzhik@ya.ru>
2024-08-05 14:32:41 +0300

    finalize `PlusesCell` sub-child index component

--- src/components/Pages/index/Grid/PlusesCell.tsx
+++ src/components/Pages/index/Grid/PlusesCell.tsx
@@ -4,7 +4,6 @@ import {cn} from '@/lib/utils'
 import {child, gap} from '##/index/Grid'
 
 import Snippet from '#/UI/Snippet'
-import copy from 'copy-to-clipboard'
 
 const blockStyles = 'bg-item rounded-2xl'
 
@@ -52,7 +51,7 @@ export default function PlusesCell() {
     }, [])
 
     return (
-      <div className={cn('row-span-4', 'p-5 pt-3 xl:p-3.5 xl:pt-2.5 sm:pt-3 space-y-5 sm:space-y-6 justify-between', blockStyles)}>
+      <div className={cn('row-span-4', 'p-5 pt-3 xl:p-3 flex flex-col gap-5 justify-between', blockStyles)}>
         <h2 className="text-[40px] xl:text-[28px] sm:text-3xl font-book tracking-tight text-primary !leading-[90%] sm:text-center sm:!leading-none">
           уже разобрали <br className="hidden sm:block" /> <span className="hidden sm:inline">все</span> компоненты <br /> для тебя
         </h2>
@@ -69,37 +68,25 @@ export default function PlusesCell() {
   function PlusesCellCode() {
     const codeData = {
       html: '<div id="block">Блок</div>',
-      css: '.block {\n  width: 100px;\n  background: purple;\n}',
+      css: '.block {\n  width: 100px;\n  height: 100px;\n  background: purple;\n}',
       js: 'console.log("Клик!");',
     }
 
-    const [copied, setCopied] = useState(false)
-
-    const handleCopy = (text: string, language: string) => {
-      copy(text, {format: 'text/plain'})
-      setCopied(true)
-      setTimeout(() => setCopied(false), 1500)
-    }
-
     const Code = ({language, className = '', code}) => (
-      <div onClick={() => handleCopy(code, language)} title="Click to copy" className={className}>
-        <Snippet lang={language} className="[&>pre]:p-4 [&>pre]:rounded-md text-xl" code={code} />
-
-        {/* {copied && (
-          <span className="absolute left-0 z-20 grid w-full sm:hidden bottom-3 place-items-center">
-            <span className="px-3 py-1 text-white rounded-md bg-[#333]">Скопировано</span>
-          </span>
-        )} */}
+      <div className={className}>
+        <Snippet lang={language} className="[&>pre]:p-3 [&>pre]:rounded-lg [&>pre]:ring-neutral-500/25 [&>pre]:ring-[1.5px] text-lg xl:text-sm sm:text-base" code={code} />
       </div>
     )
 
     return (
-      <div className={cn('row-span-6', 'relative overflow-hidden p-5 pt-3 xl:p-3.5 xl:pt-2.5 sm:pt-3 space-y-5 sm:space-y-6', blockStyles)}>
-        <h2 className="text-5xl xl:text-3xl !leading-[90%] font-book tracking-tight text-primary sm:text-center sm:!leading-none">весь код уже написан</h2>
+      <div className={cn('row-span-6', 'relative overflow-hidden p-5 pt-3 xl:p-3 sm:pb-4 flex flex-col gap-2 sm:gap-5 justify-between', blockStyles)}>
+        <h2 className="text-5xl xl:text-3xl !leading-[90%] font-book tracking-tight text-primary sm:text-center sm:!leading-none">
+          весь код <br className="hidden sm:block" /> уже написан
+        </h2>
 
-        <div className="sm:space-y-2 [&>*]:cursor-pointer">
+        <div className="relative flex flex-col justify-center h-full mx-1 sm:gap-2">
           <Code className="rotate-[-3deg] hover:rotate-[-4deg] duration-300 sm:rotate-0" language="css" code={codeData.css} />
-          <Code className="rotate-[2deg] hover:rotate-[0deg] duration-300 sm:rotate-0 mt-0.5 sm:mt-0" language="html" code={codeData.html} />
+          <Code className="rotate-[2deg] hover:rotate-[0deg] duration-300 sm:rotate-0 mt-1.5 sm:mt-0" language="html" code={codeData.html} />
           <Code className="rotate-[-3deg] hover:rotate-[-5deg] duration-300 sm:rotate-0" language="javascript" code={codeData.js} />
         </div>
       </div>
